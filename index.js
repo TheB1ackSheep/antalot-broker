84b0a4de26053fdd76497cefbc4af83c73e30617
Falook Glico <falook@Pasuths-MBP.lan>
2016-02-09 02:55:01 +0700

    arrow function

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ var server = new mosca.Server(config.mqtt_setting);
 
 i.banner();
 
-server.on('ready', function() {
+server.on('ready', () => {
   var db_conn = new Retry();
-  db_conn.start(function() {
+  db_conn.start(() => {
     i.info('Connecting to database...');
-    mongo.connect(db_url, function(err, db) {
+    mongo.connect(db_url, (err, db) => {
       if(err){
         db_conn.emit('error', 'Cannot connect to DB', new Error(err));
         return;
@@ -23,18 +23,18 @@ server.on('ready', function() {
       db_conn.emit('success', db);
     });
   });
-  db_conn.error(function(err, ex) {
+  db_conn.error((err, ex) => {
     i.error(err, ex);
-    setTimeout(function() {
+    setTimeout(() => {
       db_conn.emit('start');
     }, 1000);
 
   });
   
-  db_conn.success(function(db) {
+  db_conn.success((db) => {
     i.success("Database connected.");
     i.info("Broker is up and running.")
-    server.authenticate = function(client, username, password, callback) {
+    server.authenticate = (client, username, password, callback) => {
       var user_collection = db.collection('users');
       user_collection.find({username:username, pwd:password.toString()}).toArray(function(err, docs){
         var authorized = docs.length > 0;
@@ -42,15 +42,15 @@ server.on('ready', function() {
         callback(null, authorized);
       });
     }
-    server.authorizePublish = function(client, topic, payload, callback) {
+    server.authorizePublish = (client, topic, payload, callback) => {
       callback(null, payload);
     }
-    server.authorizeSubscribe = function(client, topic, callback) {
+    server.authorizeSubscribe = (client, topic, callback) => {
       callback(null, true);
     } 
   });  
 });
 
-server.on('clientConnected', function(client) {
+server.on('clientConnected', (client) => {
   i.info('connection accepted from', client.id);
 });
