3e5da456275f1f903d6615a001f8f7ce9074aae1
Falook Glico <Falook Glico>
2016-04-24 22:53:57 +0700

    push sensor data to db

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ var mosca = require('mosca'),
   cradle = require('cradle'),
   config = require('./config.js'),
   i = require('./log.js'),
-  Retry = require('./retry.js');
-
-// Config for database connection
-var db = new(cradle.Connection)('http://falook-dev.cloudapp.net', 5984 ).database('antalot');
+  Retry = require('./retry.js'),
+  Firebase = require("firebase");
 
 // Config for mqtt server
 var server = new mosca.Server(config.mqtt_setting);
-
-var worker;
 
 /*server.checkPermission = function(user, topic){
   var permissions = user.permissions;
@@ -28,71 +24,7 @@ i.banner();
 
 // Bind ready event for mqtt server
 server.on('ready', () => {
-  // Create Retry instance
-  var db_conn = new Retry();
-  // Start retry
-  db_conn.start(() => {
-    i.info('Connecting to database...');
-    db.exists(function (err, exists) {
-      if (err) {
-        // Emit error to retry to connect to db
-        db_conn.emit('error', 'Cannot connect to DB', err);
-      } else if (exists) {
-        db_conn.emit('success', db);
-      } else {
-        i.warn("Database is not exists.");
-        db.create();
-        i.success("Database is created.");
-        db_conn.emit('success', db);
-      }
-    });
-  });
-
-  // Error event try to reconnect to db every 1 s
-  db_conn.error((err, ex) => {
-    i.error(err, ex);
-    setTimeout(() => {
-      db_conn.emit('start');
-    }, 1000);
-  });
-
-  db_conn.success((db) => {
-    i.success("Database connected.");
-    i.success("Broker is up and running.");
-    /*server.authenticate = (client, username, password, callback) => {
-      var user_collection = db.collection('users');
-      if(username && password)
-        user_collection.find({name:username, passwd:password.toString()}).toArray(function(err, docs){
-          var authorized = docs.length > 0;
-          if (authorized) client.user = docs[0];
-          callback(null, authorized);
-        });
-      else
-        callback(null, false);
-    };
-    server.authorizePublish = (client, topic, payload, callback) => {
-      if(server.checkPermission(client.user, topic)){
-        i.success(client.id, 'published.');
-        i.info('\tTopic : ', topic);
-        i.info('\tMessage : ', payload.toString());
-        callback(null, payload);
-      }else{
-        i.warn('Unauthorize published found.');
-        i.warn('\tClient : ', client.id);
-      }
-    };
-    server.authorizeSubscribe = (client, topic, callback) => {
-      if(server.checkPermission(client.user, topic)){
-        callback(null, true);
-        i.success(client.id, 'subscribed.');
-        i.info('\tTopic : ', topic);
-      }else{
-        i.warn('Unauthorize subscribed found.');
-        i.warn('\tClient : ', client.id);
-        callback(null, false);
-      }
-    };*/
-  });
+  i.info('MQTT server is started.');
 });
 
 server.on('clientConnected', (client) => {
@@ -101,4 +33,63 @@ server.on('clientConnected', (client) => {
 
 server.on('published', function(packet, client) {
   i.info(packet.topic, packet.payload.toString());
+
+  // run check for topic convention
+  var topic = packet.topic;
+  var payload = packet.payload.toString();
+  var slugs = topic.split('/');
+  
+  // get rid of the first array elem
+  slugs.splice(0,1);
+  
+  for(key in slugs){
+    if(!slugs[key].match('^[a-z0-9-]+$'))
+      return;
+  }
+
+  // now slug is follow by convention
+  var db = new Firebase('https://antalot.firebaseio.com/');
+
+  var factory_slug = topic.split('/')[1];
+  var machine_slug = topic.split('/')[2];
+  var board_slug = topic.split('/')[3];
+  var sensor_slug = topic.split('/')[4];
+
+  /*if(topic.match('^\/[a-z0-9-]+\/[a-z0-9-]+\/[a-z0-9-]+$')){
+    // if board published its info with topic /[FACTORY_SLUG]/[MACHINE_SLUG]/[BOARD_SLUG]
+    if(payload.isJson()){
+        db.child('/factories/'+factory_slug+'/machines/'+machine_slug+'/boards/'+board_slug+'/info').update(JSON.parse(payload));
+    }else{
+      i.error('board name "'+board_slug+'" is published invalid data');
+    }
+  }else */
+
+  if(topic.match('^\/[a-z0-9-]+\/[a-z0-9-]+\/[a-z0-9-]+\/[a-z0-9-]+\/info$')){
+    // if sensor published its output /[FACTORY_SLUG]/[MACHING_SLUG]/[BOARD_SLUG]/[SENSOR_SLUG]
+    if(payload.isJson() && typeof JSON.parse(payload) === 'object'){
+      db.child('/factories/'+factory_slug+'/machines/'+machine_slug+'/boards/'+board_slug+'/sensors/'+sensor_slug+'/info').update(JSON.parse(payload));
+    }else{
+      i.error('sensor name "'+sensor_slug+'" is published invalid data');
+    }
+    
+  }else if(topic.match('^\/[a-z0-9-]+\/[a-z0-9-]+\/[a-z0-9-]+\/[a-z0-9-]+\/output$')){
+    if(payload.match('^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$')){
+        // check payload is either int or float
+        db.child('/factories/'+factory_slug+'/machines/'+machine_slug+'/boards/'+board_slug+'/sensors/'+sensor_slug+'/output').push({ timestamp: new Date().getTime(), value: payload});
+    }else{
+      i.error('sensor name "'+sensor_slug+'" is published invalid data');
+    }
+
+  }
+  
+  
 });
+
+String.prototype.isJson = function () {
+  try {
+      JSON.parse(this.toString());
+  } catch (e) {
+      return false;
+  }
+  return true;
+};
